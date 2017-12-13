24cc9f6fb2936ced4f39d33ffa86ddf209ee6ed6
Lukas <glukas@student.ethz.ch>
2017-12-13 02:47:29 +0100

    rm galois source

--- lib/galois-bin/include/Galois/DomainSpecificExecutors.h
+++
@@ -1,9 +0,0 @@
-#ifndef GALOIS_DOMAINSPECIFICEXECUTORS_H
-#define GALOIS_DOMAINSPECIFICEXECUTORS_H
-
-#include "LigraExecutor.h"
-#include "GraphChiExecutor.h"
-#include "GraphLabExecutor.h"
-#include "LigraGraphChiExecutor.h"
-
-#endif
